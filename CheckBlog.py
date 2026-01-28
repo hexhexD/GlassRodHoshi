b2cad247714f32bccbe725c543e5c4edacc2afe2
hexhexD <alvin.ye.y@gmail.com>
2026-01-28 22:27:34 +0900

    Ready to go baby

---
+++ CheckBlog.py
@@ -0,0 +1,26 @@
+﻿import os
+import requests
+import hashlib
+
+blogUrl = "https://angloco.exblog.jp"
+discordUrl = os.environ["DISCORD_WBHOOK_URL"]
+digestFile = "blogHash.txt"
+
+r = requests.get(blogUrl, timeout=10)
+r.raise_for_status()
+digest = hashlib.sha256(r.content).hexdigest()
+
+try:
+    with open(digestFile, "r") as f:
+        existingHash = f.read().strip()
+
+except FileNotFoundError:
+    existingHash = ""
+
+if (digest != existingHash):
+    msg = {"content": "New blog update detected - https://angloco.exblog.jp"}
+    resp = requests.post(discordUrl, json=msg, timeout=10)
+    resp.raise_for_status()
+
+    with open(digestFile, "w") as f:
+        f.write(digest + "\n")
